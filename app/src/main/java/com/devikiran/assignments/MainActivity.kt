b459e74561ff6cb7070a3a29bc1fed0cb71780fa
devikiran99 <devikiranshettypn4@gmail.com>
2025-07-02 12:28:46 +0530

    meetx assignment completed

--- app/src/main/java/com/devikiran/assignments/MainActivity.kt
+++ app/src/main/java/com/devikiran/assignments/MainActivity.kt
@@ -20,28 +20,11 @@ class MainActivity : ComponentActivity() {
         setContent {
             AssignmentsTheme {
                 Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
-                    Greeting(
-                        name = "Android",
+                    HomeScreen(
                         modifier = Modifier.padding(innerPadding)
                     )
                 }
             }
         }
-    }
-}
-
-@Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Text(
-        text = "Hello $name!",
-        modifier = modifier
-    )
-}
-
-@Preview(showBackground = true)
-@Composable
-fun GreetingPreview() {
-    AssignmentsTheme {
-        Greeting("Android")
     }
 }
